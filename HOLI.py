ba0b1a6db40b7e74d323da57c47213e8ce6657a1
Eloy David Alata Giraldo <112296490+EloyAlata2002@users.noreply.github.com>
2022-10-12 16:12:35 -0500

    Add files via upload

---
+++ HOLI.py
@@ -0,0 +1,11 @@
+print("Ingrese un numero")
+numero=int(input())
+print("El numero es", numero)
+print ("El doble es ", numero*2)
+print (f"El triple es {numero*3}")
+if numero>9:
+  print("el numero tiene dos digitos")
+else:
+    print("El numero tiene un digito")
+if numero>99:
+  print("El numero tiene 3 digitos")
