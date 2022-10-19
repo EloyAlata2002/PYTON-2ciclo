2b29633ca0b83d2fae7ddbc9029364758ae6568b
Eloy David Alata Giraldo <112296490+EloyAlata2002@users.noreply.github.com>
2022-10-19 17:24:15 -0500

    Delete _3_semana_10.py

--- _3_semana_10.py
+++
@@ -1,9 +0,0 @@
-def suma(N):
-    sumatoria=N**2
-    for N in range (1,N*2,2):
-        if(N%2!=0):
-            print (N)
-    print("El cuadrado del valor es: ",sumatoria)
-  
-N= int(input("Ingrese N :> "))
-suma(N)
