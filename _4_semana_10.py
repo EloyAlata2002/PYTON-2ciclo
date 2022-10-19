c0f64c19327230094fc6d7e22380d15daac70cfb
Eloy David Alata Giraldo <112296490+EloyAlata2002@users.noreply.github.com>
2022-10-19 17:45:28 -0500

    Add files via upload

---
+++ _4_semana_10.py
@@ -0,0 +1,32 @@
+def cuenta_rangos(lista):
+    contmenos40=0
+    cont40y50=0
+    cont50y60=0
+    contmas60=0
+    for peso in lista:
+        if peso<40:
+         contmenos40=contmenos40+1
+        elif peso<=50:
+         cont40y50=cont40y50+1
+        elif peso<60:
+         cont50y60=cont50y60+1
+        else:
+         contmas60=contmas60+1
+    print("menos de 40:> ",ccontmenos40)
+    print("entre de 40 y 50:> ",cont40y50)
+    print("entre de 50 y 60:> ",cont50y60)
+    print("60 o mas:> ",contmas60)
+
+lista_pesos=[]
+n=0
+while n<1 or n>50:
+ numeroalumnos=int(input("Ingrese el numero de alumnos:> "))
+for i in range (n):
+     peso=0
+     while peso<1:
+         peso=float(input("Ingrese peso:> "))
+     lista_pesos.append(peso)
+cuenta_rangos(lista_pesos)
+
+     
+
