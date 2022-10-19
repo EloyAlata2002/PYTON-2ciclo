197bb31860e3d2381de7dad70d8645cbb8972467
Eloy David Alata Giraldo <112296490+EloyAlata2002@users.noreply.github.com>
2022-10-19 16:36:53 -0500

    Add files via upload

---
+++ _1_semana_10.py
@@ -0,0 +1,10 @@
+for x in [2,3,5,7]:
+    print(x)
+for x in "Bienvenido a la upc":
+    print (x)
+numero=5
+suma=0
+while numero<=10:
+    suma=numero+suma
+    numero=numero+1
+print ("La suma es:"+str(suma))
